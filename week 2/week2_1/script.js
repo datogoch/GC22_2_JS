b663528e87c5ab52853a29a0f9c1d69029ae14c9
LAB316 Student02 <sabakevlishvili321@gmail.com>
2019-09-27 14:51:26 +0400

    my first commit in week 2

---
+++ week 2/week2_1/script.js
@@ -0,0 +1,14 @@
+function printhello(){
+console.log("go out i am not javascript");
+}
+
+function sumoftwonumbers(number1, number2){
+console.log(number1+number2);
+}
+
+function getfive(){
+    return 5;
+}
+function returnsumoftwonumber(number1, number2){
+    return number1 + number2;
+}
